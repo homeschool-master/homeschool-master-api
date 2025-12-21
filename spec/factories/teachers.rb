a2af740275981755d4e3faede9280d92e3ed98eb
Robert Masters <robertmasters2478@gmail.com>
2025-12-20 23:15:17 -0500

    robocop fixed

--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :teacher do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    nickname { "MyString" }
-    email { "MyString" }
-    phone { "MyString" }
+    first_name { 'MyString' }
+    last_name { 'MyString' }
+    nickname { 'MyString' }
+    email { 'MyString' }
+    phone { 'MyString' }
     newsletter_subscribed { false }
-    password_digest { "MyString" }
-    profile_image_url { "MyString" }
-    email_verified_at { "2025-12-20 23:08:30" }
-    email_verification_token { "MyString" }
-    password_reset_token { "MyString" }
-    password_reset_sent_at { "2025-12-20 23:08:30" }
+    password_digest { 'MyString' }
+    profile_image_url { 'MyString' }
+    email_verified_at { '2025-12-20 23:08:30' }
+    email_verification_token { 'MyString' }
+    password_reset_token { 'MyString' }
+    password_reset_sent_at { '2025-12-20 23:08:30' }
     is_active { false }
   end
 end
