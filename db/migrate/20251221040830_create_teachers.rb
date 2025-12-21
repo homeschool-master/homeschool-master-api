c6434f6edab1288a149fda535c17cc5c8ec34bfd
Robert Masters <robertmasters2478@gmail.com>
2025-12-20 23:38:32 -0500

    add constraints and indexes to teachers migration

--- db/migrate/20251221040830_create_teachers.rb
+++ db/migrate/20251221040830_create_teachers.rb
@@ -1,21 +1,26 @@
 class CreateTeachers < ActiveRecord::Migration[7.1]
   def change
-    create_table :teachers do |t|
-      t.string :first_name
-      t.string :last_name
+    create_table :teachers, id: :uuid do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :nickname
-      t.string :email
+      t.string :email, null: false
       t.string :phone
-      t.boolean :newsletter_subscribed
-      t.string :password_digest
+      t.boolean :newsletter_subscribed, default: false
+      t.string :password_digest, null: false
       t.string :profile_image_url
       t.datetime :email_verified_at
       t.string :email_verification_token
       t.string :password_reset_token
       t.datetime :password_reset_sent_at
-      t.boolean :is_active
+      t.boolean :is_active, default: true
 
       t.timestamps
     end
+    
+    add_index :teachers, :email, unique: true
+    add_index :teachers, :email_verification_token, unique: true
+    add_index :teachers, :password_reset_token, unique: true
+    add_index :teachers, :is_active
   end
 end
