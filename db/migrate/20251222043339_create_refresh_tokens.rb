f0c4a21247de025a7f18fd4370d59f04d61b9b47
Robert Masters <robertmasters2478@gmail.com>
2025-12-21 23:42:25 -0500

    update refresh token migratoin with index and constraints.

--- db/migrate/20251222043339_create_refresh_tokens.rb
+++ db/migrate/20251222043339_create_refresh_tokens.rb
@@ -1,13 +1,17 @@
 class CreateRefreshTokens < ActiveRecord::Migration[7.1]
   def change
-    create_table :refresh_tokens do |t|
-      t.references :teacher, null: false, foreign_key: true
-      t.string :token
-      t.string :jti
-      t.datetime :expires_at
+    create_table :refresh_tokens, id: :uuid do |t|
+      t.references :teacher, null: false, foreign_key: true, type: :uuid
+      t.string :token, null: false
+      t.string :jti, null: false
+      t.datetime :expires_at, null: false
       t.datetime :revoked_at
 
       t.timestamps
     end
+    
+    add_index :refresh_tokens, :token, unique: true
+    add_index :refresh_tokens, :jti, unique: true
+    add_index :refresh_tokens, :expires_at
   end
 end
--- spec/factories/refresh_tokens.rb
+++ spec/factories/refresh_tokens.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :refresh_token do
     teacher { nil }
-    token { "MyString" }
-    jti { "MyString" }
-    expires_at { "2025-12-21 23:33:39" }
-    revoked_at { "2025-12-21 23:33:39" }
+    token { 'MyString' }
+    jti { 'MyString' }
+    expires_at { '2025-12-21 23:33:39' }
+    revoked_at { '2025-12-21 23:33:39' }
   end
 end
