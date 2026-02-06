c5220985edcbfedbfc255c0269b5750a570381db
Robert Masters <robertmasters2478@gmail.com>
2026-02-05 20:25:57 -0500

    Started validation rspec testing for teacher model.

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -3,6 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  context 'validations' do
+  let(:first_name) { 'robert' }
+  let(:last_name) { 'masters' }
+  let(:email) { 'test@gmail.com' }
+  let(:password) { 'testpassword' }
+  let(:teacher) { Teacher.create!(first_name:, last_name:, email:, password:) }
+  describe 'validations' do
+    context 'when attributes are valid' do
+      it 'creates a valid teacher' do
+        expect(teacher).to be_valid
+      end
+    end
+    # context 'when attributes are invalid' do
+    #   context 'when first name is missing' do
+    #     it 'is invalid' do
+    #       expect(teacher).not_to be_valid
+    #       expect(teacher.errors).to include("can't be blank")
+    #     end
+    #   end
+    # end
   end
 end
