6f5376b6a02e4629f8d2f41680e2e7cf3e692f12
Robert Masters <robertmasters2478@gmail.com>
2026-02-11 15:30:28 -0500

    removed unecessary fields added by AI, will add them when they are needed.

--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -2,18 +2,10 @@
 
 FactoryBot.define do
   factory :teacher do
-    first_name { 'MyString' }
-    last_name { 'MyString' }
-    nickname { 'MyString' }
-    email { 'MyString' }
-    phone { 'MyString' }
-    newsletter_subscribed { false }
-    password_digest { 'MyString' }
-    profile_image_url { 'MyString' }
-    email_verified_at { '2025-12-20 23:08:30' }
-    email_verification_token { 'MyString' }
-    password_reset_token { 'MyString' }
-    password_reset_sent_at { '2025-12-20 23:08:30' }
-    is_active { false }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.unique.email }
+    password { 'password123' }
+    is_active { true }
   end
 end
