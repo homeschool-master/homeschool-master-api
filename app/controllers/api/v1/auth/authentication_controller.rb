dfab25c35e830dfa9163975e1aac8c6b6963ebee
Robert Masters <robertmasters2478@gmail.com>
2026-02-19 00:50:42 -0500

    add registration method

--- app/controllers/api/v1/auth/authentication_controller.rb
+++ app/controllers/api/v1/auth/authentication_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     module Auth
       class AuthenticationController < BaseController
-        skip_before_action :authenticate_request
+        skip_before_action :authenticate_request, only: %i[login register]
 
         def login
           result = AuthenticationService.call(params[:email], params[:password])
@@ -17,7 +17,31 @@ module Api
           end
         end
 
+        def register
+          teacher = Teacher.new(register_params)
+
+          if teacher.save
+            render_created(teacher_response(teacher))
+          else
+            render_validation_errors(teacher)
+          end
+        end
+
         private
+
+        def register_params
+          params.permit(:first_name, :last_name, :email, :password)
+        end
+
+        def teacher_response(teacher)
+          {
+            id: teacher.id,
+            first_name: teacher.first_name,
+            last_name: teacher.last_name,
+            email: teacher.email,
+            created_at: teacher.created_at
+          }
+        end
 
         def generate_tokens(teacher)
           access_token = JwtService.encode({ teacher_id: teacher.id })
