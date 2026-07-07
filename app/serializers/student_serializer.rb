035cfd834d624b0d788a5831ae0f9cdad076954f
Robert Masters <robertmasters2478@gmail.com>
2026-07-07 17:41:04 -0400

    create student serailizer

---
+++ app/serializers/student_serializer.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class StudentSerializer
+  def self.render(student)
+    new(student).to_h
+  end
+
+  def initialize(student)
+    @student = student
+  end
+
+  def to_h
+    {
+      id: @student.id,
+      teacher_id: @student.teacher_id,
+      first_name: @student.first_name,
+      last_name: @student.last_name,
+      grade_level: @student.grade_level,
+      color: @student.color,
+      profile_image_url: @student.profile_image_url,
+      is_active: @student.is_active,
+      created_at: @student.created_at
+    }
+  end
+end
